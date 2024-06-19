3677922b6b8f72e215935bac97fc26f035a00e7d
ayotomiwasalau <ayotomiwasalau@gmail.com>
2024-06-19 08:00:45 +0100

    resolve tests fail

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,9 @@ def go(args):
     logger.info("Convert last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Saving the output artifact")
     df.to_csv("clean_sample.csv", index=False)
 
